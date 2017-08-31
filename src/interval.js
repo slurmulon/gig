5a669e62b3c5ea71648d6a404592f25acac638f3
evavro <me@madhax.io>
2017-08-31 00:53:46 -0700

    :sparkles: Interval cleanup (going to break out into its own module)

--- src/interval.js
+++ src/interval.js
@@ -4,7 +4,7 @@ export class Interval {
 
   constructor (step, wait, defer = false) {
     this.step  = step
-    this.wait  = wait // TODO: allow for this to change (to support tempo)
+    this.wait  = wait
     this.state = states.pristine
     this.time  = { start: null, end: null, remaining: null, clock: null }
 
@@ -18,9 +18,7 @@ export class Interval {
   }
 
   run () {
-    this.time.start = new Date() // works (unnecesary if `next` ends up solving the `remaining` issue)
-    // this.time.clock = setInterval(this.step, this.wait) // TODO: test with setDynterval, but probably pointless since it breaks reference
-    // this.time.clock = setDynterval(this.step, this.wait) // works, sort of
+    this.time.start = new Date()
     this.time.clock = setDynterval(this.next.bind(this), this.wait)
     this.state = states.running
   }
@@ -28,10 +26,9 @@ export class Interval {
   pause () {
     if (this.state !== states.running) return
 
-    const wait    = this.time.clock.context.wait || this.wait
+    const wait = this.time.clock.context.wait || this.wait
     const elapsed = new Date() - this.time.start
 
-    // this.time.remaining = wait - (new Date() - this.time.start) // ORIG IMPL
     this.time.remaining = wait - elapsed
     this.time.clock.clear()
 
