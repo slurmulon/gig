ed3d9822c5fd359da1b9d7ac6e5e51b48b4751ef
evavro <me@madhax.io>
2018-04-08 02:17:00 -0700

    :microscope: :bug: fixed recursive step test

--- test/track-spec.js
+++ test/track-spec.js
@@ -11,19 +11,12 @@ const should = chai.should()
 chai.use(chaiThings)
 chai.use(sinonChai)
 
-// const fixtures = {
-//   slow: {
-//     json  : JSON.parse(fs.readFileSync('./test/fixtures/slow.warb.json')),
-//     audio : fs.readFileSync('./test/fixtures/slow.wav')
-//   }
-// }
-
 describe('Track', () => {
-  // FIXME: this is failing because setInterval doesn't kick off UNTIl 2000 ms, not starts and then waits until 2000 ms.
   describe('step', () => {
+    let source = fixtures.fast.json
+    let audio  = fixtures.fast.audio
+
     it('should play the current beat', done => {
-      const source = fixtures.slow.json
-      const audio  = fixtures.slow.audio
       const track  = new Track({ source, audio })
       const wait   = source.headers['ms-per-beat']
       const stop   = sinon.spy()
@@ -40,34 +33,22 @@ describe('Track', () => {
     }).timeout(0)
 
     it('should wait ms-per-beat between each step', () => {
-
     })
 
-    xit('should recursively step through the track\'s measures and beats', done => {
-      const source = fixtures.slow.json
-      const audio  = fixtures.slow.audio
-      const track  = new Track({ source, audio, tempo: 240 })
-      const limit  = track.data.length * track.data[0].length
+    it('should recursively step through the track\'s measures and beats', done => {
+      const track = new Track({ source, audio, tempo: 240 })
+      const wait  = source.headers['ms-per-beat']
 
-      let steps  = 0
-      let passes = false
-
-      track.on.step.start = beat => {
-        console.log('stepped through beat', beat.data)
-        console.log('steps taken so far', steps)
-
-        steps++
-      }
-
-      track.on.step.stop = beat => {
-        if (beat && steps === limit) {
-          true.should.equal(true)
-          done()
-        }
-      }
+      track.step = sinon.spy()
 
       track.start()
-    })//.timeout(0)
+
+      setTimeout(() => {
+        track.step.should.have.been.calledThrice
+
+        done()
+      }, wait * 2)
+    }).timeout(0)
   })
 
   describe('play', () => {
