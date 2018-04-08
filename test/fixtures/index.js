b3308c0ec835fd0efaf009b2a3c2616d23894091
evavro <me@madhax.io>
2018-04-07 20:39:12 -0700

    :microscope: :sparkles: updated reference to test fixture

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -2,7 +2,7 @@ import fs from 'fs'
 
 export default {
   slow: {
-    json  : JSON.parse(fs.readFileSync('./test/fixtures/slow.warb.json')),
+    json  : JSON.parse(fs.readFileSync('./test/fixtures/slow.bach.json')),
     audio : fs.readFileSync('./test/fixtures/slow.wav')
   }
 }
