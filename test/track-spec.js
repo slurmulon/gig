8e35ca5a982b8f120a81570003b6b669dbe198e1
evavro <me@madhax.io>
2018-04-14 00:58:54 -0700

    :microscope: :sparkles:

--- test/track-spec.js
+++ test/track-spec.js
@@ -36,6 +36,7 @@ describe('Track', () => {
       const track = new Track({ source, audio })
       const wait = source.headers['ms-per-beat']
       const steps = 3
+      const duration = wait * steps
       const startTime = Date.now()
       let lastTime
 
@@ -46,18 +47,19 @@ describe('Track', () => {
       track.start()
 
       setTimeout(() => {
-        const targetTime = startTime + (wait * steps)
+        const targetTime = startTime + duration
 
         lastTime.should.be.closeTo(targetTime, 10)
 
         done()
-      }, (wait * steps) + 5)
+      }, duration + 5)
     }).timeout(0)
 
     it('should recursively step through the track\'s measures and beats', done => {
       const track = new Track({ source, audio, tempo: 240 })
       const wait  = source.headers['ms-per-beat']
       const steps = 2
+      const duration = wait * steps
 
       track.step = sinon.spy()
 
@@ -67,13 +69,14 @@ describe('Track', () => {
         track.step.should.have.been.calledThrice
 
         done()
-      }, wait * steps)
+      }, duration)
     }).timeout(0)
 
     it('should call the step\'s stop callback at the end of each beat', done => {
       const track = new Track({ source, audio, tempo: 240 })
       const wait  = source.headers['ms-per-beat']
       const steps = 2
+      const duration = wait * steps
 
       track.on.step.stop = sinon.spy()
 
@@ -83,11 +86,15 @@ describe('Track', () => {
         track.on.step.stop.should.have.been.calledTwice
 
         done()
-      }, wait * steps)
+      }, duration)
     }).timeout(0)
   })
 
   describe('play', () => {
+
+  })
+
+  describe('start', () => {
 
   })
 
