712e260785176fff4957499d083058b7714da98e
evavro <me@madhax.io>
2019-05-25 23:30:12 -0400

    :microscope: :wrench: Use void.wav file in test fixtures

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -3,10 +3,10 @@ import fs from 'fs'
 export default {
   fast: {
     json : JSON.parse(fs.readFileSync('./test/fixtures/fast.bach.json')),
-    // audio : fs.readFileSync('./test/fixtures/fast.wav')
+    audio : fs.readFileSync('./test/fixtures/void.wav')
   },
   slow: {
     json  : JSON.parse(fs.readFileSync('./test/fixtures/slow.bach.json')),
-    audio : fs.readFileSync('./test/fixtures/slow.wav')
+    audio : fs.readFileSync('./test/fixtures/void.wav')
   }
 }
